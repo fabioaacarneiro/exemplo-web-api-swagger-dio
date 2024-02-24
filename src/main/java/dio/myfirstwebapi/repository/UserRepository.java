08f19a1c15eccc9e0afd843845bb6b37d1867f07
Fabio Carneiro <fabioaacarneiro@gmail.com>
2024-02-24 09:35:54 -0300

    :sparkles: :feat fake repository UserRepository

---
+++ src/main/java/dio/myfirstwebapi/repository/UserRepository.java
@@ -0,0 +1,46 @@
+package dio.myfirstwebapi.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import dio.myfirstwebapi.model.Usuario;
+
+@Repository
+public class UserRepository {
+
+    public void save(Usuario usuario) {
+        if (usuario.getId() == null) {
+            System.out.println("SAVE - Recebendo o usuário na camada de repositório");
+        } else {
+            System.out.println("UPDATE - Recebendo o usuário na camada de repositório");
+        }
+
+        System.out.println(usuario);
+    }
+
+    public void deleteById(Integer id) {
+        System.out.println(String.format("DELETE/id - Recebendo o id: $d para excluir um usuário", id));
+        System.out.println(id);
+    }
+
+    public List<Usuario> findAll() {
+        System.out.println("LIST - Listando os usuários do sistema");
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("fabio", "senha"));
+        usuarios.add(new Usuario("jonas", "senha"));
+        return usuarios;
+    }
+
+    public Usuario findById(Integer id) {
+        System.out.println(String.format("FIND/id - Recebendo o id: %d para localizar um usuário", id));
+        return new Usuario("benedito", "senha");
+    }
+
+    public Usuario findByUsername(String username) {
+        System.out
+                .println(String.format("FIND/username - Recebendo o username: %s para logalizar um usuário", username));
+        return new Usuario(username, "senha");
+    }
+}
